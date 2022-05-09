0880255111dc47caa81a8bc82537f43d11bc4f75
Shaun Hamilton <shauhami020@gmail.com>
2022-05-09 21:41:35 +0100

    feat: add options with logLevel

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,44 @@
-export default function logfull(...args: any[]): void {
-  console.log(...args);
+const LogLevel = {
+  debug: 0,
+  info: 1,
+  warn: 2,
+  error: 3,
+};
+
+const options = {
+  // @ts-expect-error Error from browser build
+  logLevel: process?.env?.LOG_LEVEL || LogLevel.info,
+};
+
+export default function Logfull(ops?: typeof options) {
+  if (ops) {
+    Object.keys(ops).map((key) => {
+      if (!options.hasOwnProperty(key)) {
+        throw new Error(`Logfull \nUnknown option: ${key}`);
+      }
+      options[key] = ops[key];
+    });
+  }
 }
 
 export function info(...args: any[]): void {
-  console.info("🔵 ", ...args);
+  if (LogLevel[options.logLevel] <= LogLevel.info) {
+    console.info("🔵%cINFO: ", "color: blue", ...args);
+  }
 }
-
 export function warn(...args: any[]): void {
-  console.warn("🟠 ", ...args);
+  if (LogLevel[options.logLevel] <= LogLevel.warn) {
+    console.warn("🟠%cWARN: ", "color: orange", ...args);
+  }
 }
-
 export function error(...args: any[]): void {
-  console.error("🔴 ", ...args);
+  if (LogLevel[options.logLevel] <= LogLevel.error) {
+    console.error("🔴%cERROR: ", "color: red", ...args);
+  }
 }
-
 export function debug(...args: any[]): void {
-  console.debug("🟢 ", ...args);
-}
-
-export enum Logger {
-  INFO = "info",
-  DEBUG = "debug",
-  WARN = "warn",
-  ERROR = "error",
+  if (LogLevel[options.logLevel] === LogLevel.debug) {
+    // console.trace("[DEBUG]");
+    console.debug("🟢%cDEBUG: ", "color: green", ...args);
+  }
 }

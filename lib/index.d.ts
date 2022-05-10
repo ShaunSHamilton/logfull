c4908f8bd9f6106605d0a4b425d4c58aadad0845
Shaun Hamilton <shauhami020@gmail.com>
2022-05-10 02:07:29 +0100

    fix(patch): actually deploy lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,55 @@
+/**
+ * Default options for the logs.
+ */
 declare const options: {
+    /**
+     * @default "info"
+     */
     level: string | number;
+    /**
+     * @default "🔵INFO: "
+     */
     info: string;
+    /**
+     * @default "🟠WARN: "
+     */
     warn: string;
+    /**
+     * @default "🔴ERROR: "
+     */
     error: string;
+    /**
+     * @default "🟢DEBUG: "
+     */
     debug: string;
 };
-export default function Logfull(ops?: Partial<typeof options>): void;
+/**
+ * Sets the options for the logs.
+ *
+ * @example
+ * logover({ level: LogLevel.debug });
+ *
+ * @param ops Options to override the default options.
+ */
+export default function Logover(ops?: Partial<typeof options>): void;
+/**
+ * Equivalent to `console.info`.
+ * @param args The arguments to log.
+ */
 export declare function info(...args: any[]): void;
+/**
+ * Equivalent to `console.warn`.
+ * @param args The arguments to log.
+ */
 export declare function warn(...args: any[]): void;
+/**
+ * Equivalent to `console.error`.
+ * @param args The arguments to log.
+ */
 export declare function error(...args: any[]): void;
+/**
+ * Equivalent to `console.debug`.
+ * @param args The arguments to log.
+ */
 export declare function debug(...args: any[]): void;
 export {};

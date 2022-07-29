535845619a29a26290ba63ac4cc2a5626e580d43
Shaun Hamilton <shauhami020@gmail.com>
2022-07-30 00:49:07 +0100

    fix(1.3.4): build with type declarations

---
+++ lib/error.d.ts
@@ -0,0 +1,7 @@
+export declare class LogError extends Error {
+    stackTraceDepth: number;
+    constructor(message: string);
+    get stacks(): string;
+    set stackDepth(depth: number);
+}
+//# sourceMappingURL=error.d.ts.map
